e3bb353f1fe9f97a591d2e02382bb6bb66b3c1dc
pierre.blarre@gmail.com <pierre.blarre@gmail.com>
2018-01-14 18:18:29 +0100

    SempicOnto getters & setters

--- src/main/java/fr/uga/miashs/sempic/model/rdf/SempicOnto.java
+++ src/main/java/fr/uga/miashs/sempic/model/rdf/SempicOnto.java
@@ -24,4 +24,71 @@ public class SempicOnto {
 	public static Resource Animal;
 	public static Resource Depiction;
 	
+	public SempicOnto() {
+	}
+
+	public static Resource getPhoto() {
+		return Photo;
+	}
+
+	public static void setPhoto(Resource Photo) {
+		SempicOnto.Photo = Photo;
+	}
+
+	public static Property getAlbumId() {
+		return albumId;
+	}
+
+	public static void setAlbumId(Property albumId) {
+		SempicOnto.albumId = albumId;
+	}
+
+	public static Property getOwnerId() {
+		return ownerId;
+	}
+
+	public static void setOwnerId(Property ownerId) {
+		SempicOnto.ownerId = ownerId;
+	}
+
+	public static String getDepicts() {
+		return depicts;
+	}
+
+	public static void setDepicts(String depicts) {
+		SempicOnto.depicts = depicts;
+	}
+
+	public static String getTakenBy() {
+		return takenBy;
+	}
+
+	public static void setTakenBy(String takenBy) {
+		SempicOnto.takenBy = takenBy;
+	}
+
+	public static String getTakenIn() {
+		return takenIn;
+	}
+
+	public static void setTakenIn(String takenIn) {
+		SempicOnto.takenIn = takenIn;
+	}
+
+	public static Resource getAnimal() {
+		return Animal;
+	}
+
+	public static void setAnimal(Resource Animal) {
+		SempicOnto.Animal = Animal;
+	}
+
+	public static Resource getDepiction() {
+		return Depiction;
+	}
+
+	public static void setDepiction(Resource Depiction) {
+		SempicOnto.Depiction = Depiction;
+	}
+	
 }
