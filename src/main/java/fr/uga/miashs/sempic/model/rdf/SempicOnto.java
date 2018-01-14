ecff6d1d634c2ed36700965c450128905fb9a7ca
pierre.blarre@gmail.com <pierre.blarre@gmail.com>
2018-01-14 18:10:37 +0100

    Starting to add RDF and Ontology to project

---
+++ src/main/java/fr/uga/miashs/sempic/model/rdf/SempicOnto.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uga.miashs.sempic.model.rdf;
+
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
+/**
+ *
+ * @author Pierre Blarre <Pierre.Blarre@etu.univ-grenoble-alpes.fr>
+ */
+public class SempicOnto {
+
+	public static Resource Photo;
+	public static Property albumId;
+	public static Property ownerId;
+	public static String depicts;
+	public static String takenBy;
+	public static String takenIn;
+	public static Resource Animal;
+	public static Resource Depiction;
+	
+}
